6dd969280f3901657b49dc7934574540dd8ebb01
Puneet-Chicmic <puneet.nanda@chicmic.co.in>
2022-04-13 09:43:59 +0530

    dev commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
 import Hello from './Hello'
+import Bte from './Bte'
 
 const App = () => {
   return (
     <SafeAreaView>
       <Hello/>  
+      <Bte/>
     </SafeAreaView>
   )
 }
---
+++ Bte.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Bte = () => {
+    return (
+        <View>
+            <Text>byee byeee</Text>
+        </View>
+    )
+}
+
+export default Bte
